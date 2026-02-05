b88894289ff8550ba7573685d947b6df11bc4406
truanthh <aquapyr@gmail.com>
2026-02-05 18:51:21 +0300

    dlksjf

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,16 +1,13 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
--- config.font =
--- wezterm.font('Consolas Nerd Font Mono', { weight = "Bold" })
- 
---config.font =
--- wezterm.font('Consolas Nerd Font Mono', { }) 
+config.font = wezterm.font("Monaspace Neon NF")
+-- config.font = wezterm.font("CaskaydiaCove NF")
 
 -- For example, changing the color scheme:
 -- config.color_scheme = 'Bamboo'
@@ -19,14 +16,14 @@ local config = wezterm.config_builder()
 config.hide_tab_bar_if_only_one_tab = true
 
 config.colors = {
-  -- The default text color
-  foreground = '#2E3440',
-  -- The default background color
-  background = '#F7F8FA',
+	-- The default text color
+	foreground = "#2E3440",
+	-- The default background color
+	background = "#F7F8FA",
 }
 
 config.keys = {
-    {key = 'Slash', mods = 'CTRL', action = wezterm.action{SendString = '\x1f'}},
+	{ key = "Slash", mods = "CTRL", action = wezterm.action({ SendString = "\x1f" }) },
 }
 
 -- and finally, return the configuration to wezterm
